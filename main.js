385d0e0e2541d95eacf44ff05e2b4c320496238e
= <=>
2022-01-06 13:00:50 +0600

    List rendering

--- main.js
+++ main.js
@@ -2,10 +2,15 @@ const app = Vue.createApp({
      data() {
           return {
                product: "Socks",
-               details: "This is important for show",
                image: './assets/images/socks_blue.jpg ',
                url: 'http://yeahyea.netlify.app',
-               inStock: 0
+               inStock: 0,
+               details: ['50% cotton', '30% wool', '20% polyester'],
+               names: ['Yeahyea Saker', 'Sumaya Hamida','Morium Jamila', 'Tahmina Yeasmin'],
+               variants: [
+                    {id: 2234, color: 'green'},
+                    {id: 2235, color: 'blue'},
+               ]
           }
      }
 })
