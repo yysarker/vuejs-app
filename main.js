93b085e72c836143f96bd0f9d0c0445ce501bf24
= <=>
2022-01-06 09:27:04 +0600

    Create vue app and show the product also use the v-bind

---
+++ main.js
@@ -0,0 +1,10 @@
+const app = Vue.createApp({
+     data() {
+          return {
+               product: "Socks",
+               details: "This is important for show",
+               image: './assets/images/socks_green.jpg',
+               url: 'http://yeahyea.netlify.app'
+          }
+     }
+})
